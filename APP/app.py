36ac8440ff4b75fcfe145db8993d08f6509587b3
ajmarrugos <ajmarrugos@gmail.com>
2022-10-15 19:24:42 -0500

    Added timebar stamp function

--- APP/app.py
+++ APP/app.py
@@ -5,18 +5,19 @@
 import requests
 from time import sleep
 from var_pack import msg
+from datetime import datetime
 
-# wait time controller
-sleep_time = 2
+# Wait time controller
+sleep_time = 1
 
 # Classes to process
 classes = ['Offer']
 
-# Constants1
+# Constants
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 pages = ['https://mx.computrabajo.com/', 'https://co.computrabajo.com/']
-currencies = ['MXP', 'COP']
 countries = ['Mexico', 'Colombia']
+currencies = ['MXP', 'COP']
 
 # Temporal Variables
 cur_pag = ""
@@ -26,16 +27,22 @@ job_search = ""
 num_offers = ""
 my_url = ""
 
+def time_bar():
+    now = datetime.now()
+    ct = now.strftime("%H:%M:%S")
+    print("\n" + msg.bar + ct + "\n")
 
-def welcome():
-    """ Prints a Weolcome Message to the screen """
-    print("\n" + msg.welcome)
-    print("\n" + msg.intro)
+def step0():
+    """ Prints a Welcome Message to the screen """
+    print("\n" + msg.bar + "\n\n" + msg.welcome)
+    print(msg.intro1)
+    print(msg.intro2)
 
 def step1():
-    """ Method used to select a web page to look into """
+    """ Method used to select the URL WebScrapper to use """
     global cur_pag;
-    print("\n" + msg.quest1)
+    time_bar()
+    print(msg.quest1)
 
     for i, page in enumerate(pages):
         print(i, page)
@@ -54,9 +61,10 @@ def step1():
         pass
 
 def step2():
-    """ Method used to select the country where to look for offers """
+    """ Method used to select the country to set a currency """
     global cur_country, cur_currency;
-    print("\n" + msg.quest2)
+    time_bar()
+    print(msg.quest2)
 
     for i, country in enumerate(countries):
         print(i, country)
@@ -80,22 +88,24 @@ def step2():
 
 def step3():
     """ Method used to get the Position or Role to search """
-    global job_search;
-    print("\n" + msg.quest3)
+    global job_search, my_url;
+    time_bar()
+    print(msg.quest3)
 
     answer = str(input("\n" + "R// "))
 
     if answer != 'null':
         job_search = answer
         print(msg.typed + job_search)
-
+        my_url = cur_pag + "trabajo-de-" + job_search
     else:
         pass
 
 def step4():
     """ Method used to get the number of offers to request """
     global num_offers;
-    print("\n" + msg.quest4)
+    time_bar()
+    print(msg.quest4)
 
     answer = int(input("\n" + "R// "))
 
@@ -106,21 +116,22 @@ def step4():
     else:
         pass
 
-def urL_builder():
-    """ Method used to get the number of offers to request """
-    global cur_pag, job_search, my_url, headers;
-    print("\n" + msg.got_url)
-    my_url = cur_pag + "trabajo-de-" + job_search
-    print(my_url)
+def url_builder():
+    """ Function used to build a UR """
+    global cur_pag, job_search, num_offers, my_url, headers;
+    time_bar()
+    print(msg.search_for + str(num_offers) + " offers")
+    print(msg.got_url + my_url)
 
     print("\n" + msg.quest5)
     answer = input("\n" + "R// ")
 
     if answer == 'y' or 'yes':
-        print("\n" + msg.working)
+        print(msg.bar + msg.working)
         sleep(sleep_time)
+
         r = requests.get(my_url, headers=headers)
-        print("\n" + msg.got_html)
+        print("\n" + msg.got_html + "\n" + msg.bar)
         sleep(sleep_time)
         print(r.content)
 
@@ -129,19 +140,9 @@ def urL_builder():
         step1()
 
 # def make_request():
-welcome();
-
+step0();
 step1();
-print(cur_pag)
-
 step2();
-print(cur_country + " = " + cur_currency)
-
 step3();
-print("Your are looking for: " + job_search)
-
 step4();
-print("You are expecting for: " + str(num_offers) + " results")
-
-urL_builder();
-print(my_url)
+url_builder();
--- APP/var_pack/msg.py
+++ APP/var_pack/msg.py
@@ -1,20 +1,24 @@
 # UI Messages
-welcome = "Hello! I'm your JobSeeker Bro!" 
-intro = "My job is to save time on your job seeking search." + "\n" + "I'll look into our 'available search pages' to get you the most offers as possible."
-
-# Notifications
-selection = "You selected: "
-bad_selection = "Wrong option. Please try again..."
-typed = "You typed: "
-working = "Processing..."
-currency_format = "From now, salary results will be shown as: "
-got_url = "Excellent, now that the process is complete, it's time to you to confirm this search: "
-got_html = "Ok... Here it comes..."
+bar = "----------------------------- "
+welcome = "Hello! I'm JobSeeker Bro!" 
+intro1 = "My purpose is to help you save time on your job seeking journey."
+intro2 = "I'll look into our 'available search pages' to get you the most offers as possible."
 
 # Step Questions
 quest1 = "STEP 1: Please select one of our main pages to look into:"
 quest2 = "STEP 2: Please select the country where you want to work:"
 quest3 = "STEP 3: Please type the Position or Role you want to search for:"
 quest4 = "STEP 4: Please type the number of offers you want to get:"
-quest5 = "CONFIRM: Sure you want me to proceed?:"
+quest5 = "Please confirm if you want to proceed?"
 
+# Notifications
+selection = "You selected: "
+typed = "You typed: "
+currency_format = "From now, salary results will be shown as: "
+search_for = "Excellent, just to confirm that we're going to search for: "
+got_url = "And make a request to the following URL: "
+
+# Status
+working = "Processing..."
+bad_selection = "Wrong option. Please try again..."
+got_html = "Ok... Here it comes..."
