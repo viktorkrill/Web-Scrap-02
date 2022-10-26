93f5d06d6955710b75c4c2c0949ec066a837fd91
alejuran <alejandrourn@gmail.com>
2022-10-26 15:52:00 -0500

    dbstorage

---
+++ cl-computrabajo/db/models/job_offer.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Job Offer class"""
+import uuid
+from ..db_storage import Base
+from datetime import datetime
+
+from sqlalchemy import Column, String, DateTime
+
+class JobOffer(Base):
+    """ The city class, contains state ID and name """
+    __tablename__ =  "offers"
+
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        d = self.__dict__.copy()
+        d.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
+
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.new(self)
+        storage.save()
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = dict(self.__dict__)
+        dictionary["__class__"] = str(type(self).__name__)
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary.pop("_sa_instance_state", None)
+        return dictionary
+
+    def delete(self):
+        """Deletes object"""
+        from models import storage
+        storage.delete(self)
+
+
+    url_empleo = Column(String(128))
+    fecha_recuperacion = Column(String(60))
+    Titulo_oferta = Column(String(60))
+    Empresa = Column(String(60))
+    Ubicacion = Column(String(60))
+    Descripcion = Column(String(1000))
+    Requerimientos = Column(String(256))
+    Salario = Column(String(60))
