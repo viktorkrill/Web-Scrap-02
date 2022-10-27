b1e0034eda47dae1d8921e3d83aba69b3f28da7a
Mr-emilio <pablopeag@hotmail.com>
2022-10-26 20:54:53 -0500

    deleted draft files

--- APP/models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# Initialize the models package
-
-from os import getenv
-from models.engine.db import DBStorage
-
-storage_t = getenv("HITCH_TYPE_STORAGE")
-storage = DBStorage()
